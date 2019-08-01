aafe6833d82480757b5993e45717c412c3cc1fc8
Shurui Liu <shurui91@gmail.com>
2019-08-01 10:21:55 -0700

    lecture 81

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,5 +1,31 @@
 var budgetController = (function() {
 	// some code
+	var Expense = function(id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
+
+	var Income = function(id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
+
+	var allExpenses = [];
+	var allIncomes = [];
+	var totalExpenses = 0;
+
+	var data = {
+		allItems: {
+			exp: [],
+			inc: []
+		},
+		totals: {
+			exp: 0,
+			inc: 0
+		}
+	};
 })();
 
 var UIController = (function() {
