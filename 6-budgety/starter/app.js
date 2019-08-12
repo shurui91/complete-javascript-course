8fc6bcf8a1eac5891ced37ef6e5da245a5c5091a
Shurui Liu <shurui91@gmail.com>
2019-08-11 20:30:10 -0700

    done with lecture 83

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -64,7 +64,9 @@ var UIController = (function() {
 		inputType: '.add__type',
 		inputDescription: '.add__description',
 		inputValue: '.add__value',
-		inputButton: '.add__btn'
+		inputButton: '.add__btn',
+		incomeContainer: '.income__list',
+		expensesContainer: 'expenses__list'
 	};
 
 	return {
@@ -75,6 +77,28 @@ var UIController = (function() {
 				value: document.querySelector(DOMStrings.inputValue).value
 			};
 		},
+
+		addListItem: function(obj, type) {
+			var html, newHtml, element;
+			// create HTML string with placeholder text
+			if (type === 'inc') {
+				element = DOMStrings.incomeContainer;
+				html =
+					'<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			} else if (type === 'exp') {
+				element = DOMStrings.expensesContainer;
+				html =
+					'<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			}
+
+			// replace the placeholder text with some actual data
+			newHtml = html.replace('%id%', obj.id);
+			newHtml = newHtml.replace('%description%', obj.description);
+			newHtml = newHtml.replace('%value%', obj.value);
+			// insert the HTML into DOM
+			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+		},
+
 		getDOMStrings: function() {
 			return DOMStrings;
 		}
@@ -100,10 +124,15 @@ var controller = (function(budgetController, UIController) {
 		// 1. get the field input data
 		input = UIController.getInput();
 		console.log(input);
+
 		// 2. add the item to the budget controller
 		newItem = budgetController.addItem(input.type, input.description, input.value);
+
 		// 3. add the item to the UI
+		UIController.addListItem(newItem, input.type);
+
 		// 4. calculate the budget
+
 		// 5. display the budget on the UI
 		console.log('it works');
 	};
